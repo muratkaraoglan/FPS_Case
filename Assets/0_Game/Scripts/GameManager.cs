99eefc7314116d9f39e68ca8aa9de3e6b56d602b
muratkaraoglan <m.karaoglan1604@gmail.com>
2024-04-01 17:11:08 +0300

    cursor visible

--- Assets/0_Game/Scripts/GameManager.cs
+++ Assets/0_Game/Scripts/GameManager.cs
@@ -9,12 +9,15 @@ public class GameManager : Singleton<GameManager>
 
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
         Time.timeScale = 1f;
         FPSController.OnPlayerDie += FPSController_OnPlayerDie;
     }
 
     private void FPSController_OnPlayerDie()
     {
+        Cursor.visible = true;
         Time.timeScale = 0f;
     }
 }
